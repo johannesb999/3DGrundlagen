e3bd2f6b1d2a64219713e0f8d27cedfb7e352a70
Johannes Biess <128609732+johannesb999@users.noreply.github.com>
2023-10-08 11:50:05 +0200

    zwischenupdate

--- Sketch.js
+++ Sketch.js
@@ -10,7 +10,6 @@ var projection = d3
   .scale(150)
   .center([0, 20])
   .translate([width / 2, height / 2]);
-
 // Data and color scale
 var data = d3.map();
 var colorScale = d3
@@ -24,20 +23,27 @@ d3.queue()
   .defer(d3.csv, "Data/gini.csv")
   .await(ready);
 
-function ready(error, topo) {
+function ready(error, topo, gini) {
+  // Hier wird die GINI-Map aus den geladenen Daten erstellt
+  var giniMap = d3.map();
+  gini.forEach(function (d) {
+    giniMap.set(d["Country Code"], +d["Value"]);
+  });
+
   let mouseOver = function (d) {
-    d3.selectAll(".Country").transition().duration(200).style("opacity", 0.5);
+    d3.selectAll(".Country").transition().duration(10).style("opacity", 0.5);
     d3.select(this)
       .transition()
-      .duration(200)
+      .duration(10)
       .style("opacity", 1)
       .style("stroke", "black");
   };
 
   let mouseLeave = function (d) {
-    d3.selectAll(".Country").transition().duration(100).style("opacity", 0.5);
-    d3.select(this).transition().duration(200).style("stroke", "transparent");
+    d3.selectAll(".Country").transition().duration(10).style("opacity", 0.5);
+    d3.select(this).transition().duration(10).style("stroke", "transparent");
   };
+
   // Draw the map
   svg
     .append("g")
@@ -49,8 +55,8 @@ function ready(error, topo) {
     .attr("d", d3.geoPath().projection(projection))
     // set the color of each country
     .attr("fill", function (d) {
-      d.total = data.get(d.id) || 0;
-      return colorScale(d.total);
+      var giniValue = giniMap.get(d.id);
+      return colorScale(giniValue);
     })
     .style("stroke", "transparent")
     .attr("class", function (d) {
