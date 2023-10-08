5a29fa4358573109e6b291e043999b86c3ed130c
Johannes Biess <128609732+johannesb999@users.noreply.github.com>
2023-10-09 01:08:59 +0200

    update

--- Sketch.js
+++ Sketch.js
@@ -2,7 +2,9 @@
 d3.queue()
   .defer(d3.json, "Data/mapdata.json")
   .defer(d3.csv, "Data/gini.csv")
+  .defer(d3.csv, "Data/crimeRate.csv")
   .await(ready);
+
 // The svg
 var svg = d3.select("svg"),
   width = +svg.attr("width"),
@@ -15,6 +17,7 @@ var projection = d3
   .scale(150)
   .center([0, 20])
   .translate([width / 2, height / 2]);
+
 // Data and color scale
 var data = d3.map();
 var colorScale = d3
@@ -22,11 +25,19 @@ var colorScale = d3
   .domain([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]) // Schwellenwerte im Bereich von 1 bis 100
   .range(d3.schemeBlues[7]);
 
-function ready(error, topo, gini) {
+var gini;
+
+function ready(error, topo, giniData, crimeData) {
   // Hier wird die GINI-Map aus den geladenen Daten erstellt
+  gini = giniData;
   var giniMap = d3.map();
   gini.forEach(function (d) {
     giniMap.set(d["Country Code"], +d["Value"], +d["Year"], +d["Country Name"]);
+  });
+
+  var crimeMap = d3.map();
+  crimeData.forEach(function (d) {
+    crimeMap.set(d["Country"], +d["VALUE"]);
   });
 
   let mouseOver = function (d) {
@@ -42,16 +53,7 @@ function ready(error, topo, gini) {
     d3.selectAll(".Country").transition().duration(10).style("opacity", 0.5);
     d3.select(this).transition().duration(10).style("stroke", "transparent");
   };
-  function clickCountry(d) {
-    // Hier können Sie die Daten des geklickten Landes in der Konsole anzeigen
-    var countryCode = d.properties["ISO_A2"];
-    var giniValue = giniMap.get(countryCode);
-    var countryName = d.properties["NAME"];
 
-    console.log("Country Code: " + countryCode);
-    console.log("Country Name: " + countryName);
-    console.log("Gini Value: " + giniValue);
-  }
   // Draw the map
   svg
     .append("g")
@@ -76,38 +78,71 @@ function ready(error, topo, gini) {
     .on("mouseover", mouseOver)
     .on("mouseleave", mouseLeave);
 }
+//Draw the circles
+svg
+  .append("g")
+  .selectAll("circle")
+  .data(topo.features)
+  .enter()
+  .append("circle")
+  .attr("transform", function (d) {
+    var centroid = projection(d3.geoCentroid(d));
+    return "translate(" + centroid + ")";
+  })
+  .attr("r", function (d) {
+    var crimeRate = crimeMap.get(d.properties["NAME"]);
+    return crimeRate ? crimeRate / 10 : 0; // Skalieren Sie die Kreisgröße nach Bedarf
+  })
+  .attr("fill", "yellow"); // Setzen Sie die Füllfarbe der Kreise
 
 // Funktion zur Aktualisierung der GINI-Map basierend auf dem ausgewählten Jahr
 function updateYear(selectedYear) {
-  // Das ausgewählte Jahr im HTML-Dokument aktualisieren
-  var slider = document.getElementById("yearSlider");
-  document.getElementById("selectedYear").textContent =
-    "Selected Year: " + selectedYear;
-
-  // Daten filtern, um nur Daten für das ausgewählte Jahr zu behalten
   var filteredData = gini.filter(function (d) {
     return +d["Year"] === +selectedYear;
   });
 
   // Ein leeres Objekt erstellen, um die GINI-Werte pro Land zu speichern
   var giniValues = {};
-
   // Daten in das Objekt speichern
   filteredData.forEach(function (d) {
     giniValues[d["Country Code"]] = +d["Value"];
   });
-
   // Daten neu zeichnen
   svg.selectAll("path").attr("fill", function (d) {
-    // GINI-Wert für das ausgewählte Land finden
     var giniValue = giniValues[d.id];
-
-    // Wenn ein Wert gefunden wurde, die Kartenfarbe aktualisieren
     if (giniValue) {
       return colorScale(giniValue);
     } else {
-      // Wenn kein Wert gefunden wurde, eine Standardfarbe verwenden
-      return "gray"; // Ändern Sie dies nach Bedarf
+      return "white";
     }
   });
 }
+function updateCrimeYear(selectedYear) {
+  var filteredCrimeData = crimeData.filter(function (d) {
+    return +d["Year"] === +selectedYear;
+  });
+
+  // Eine leere Map erstellen, um die Kriminalitätsraten pro Land zu speichern
+  var crimeRates = {};
+
+  // Daten in die Map speichern
+  filteredCrimeData.forEach(function (d) {
+    crimeRates[d["Country"]] = +d["VALUE"];
+  });
+
+  // Kreise neu zeichnen
+  svg.selectAll("circle").attr("r", function (d) {
+    var crimeRate = crimeRates[d.properties["NAME"]];
+    if (crimeRate) {
+      return crimeRate / 10; // Skalieren Sie die Kreisgröße nach Bedarf
+    } else {
+      return 0;
+    }
+  });
+}
+
+var tooltip = d3
+  .select("body")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
